0429b22a083f67f16672e37901494c8b20774557
rkobus <radek109@o2.pl>
2023-11-15 19:02:32 +0100

    Test seedera

---
+++ JobHub/Migrations/20231115175925_Wyczyszczenie_bazy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobHub.Migrations
+{
+    /// <inheritdoc />
+    public partial class Wyczyszczenie_bazy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
